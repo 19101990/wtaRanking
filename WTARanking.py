b18762c7fe2f7034e6cbbd07bd2b0d88dd8a2a86
19101990 <exo.gf.s@gmail.com>
2018-05-03 11:20:55 +0200

    add functions: findPlayerByRanking, findPlayerByCountry, findPlayerByName

--- WTARanking.py
+++ WTARanking.py
@@ -16,11 +16,52 @@ soup = bs.BeautifulSoup(sauce, 'lxml')
 ranking_table = soup.find('tbody')
 
 ranking_table_rows = ranking_table.find_all('tr')
-
+rows = []
 for tr in ranking_table_rows:
     td = tr.find_all('td')
-    rows = []
     for i in td:
-        rows.append(i.text)
-    print(rows[0].strip() + " " + rows[2].strip().upper() +
-          "\n\tPoints: " + rows[4].strip() + "\n")
+        rows.append(i.text.strip()) # get rid of white spaces
+
+
+# group each 5 items in the list in a new list
+#(current position[0], previous position[1], name[2], country[3], points[4])
+players = [rows[n:n+5] for n in range(0,len(rows), 5)]
+
+
+# find player by ranking
+def findPlayerByRanking():
+    input_ranking = int(input("Enter ranking: "))
+    if input_ranking == 0 or input_ranking > 200:
+        print("Note: you can only search for players in range from 1 to 200")
+    else:
+        input_ranking-=1
+        print(players[input_ranking][2])
+
+
+# find player by country
+def findPlayerByCountry():
+    input_country = str(input("Enter country name: "))
+    for player in players:
+        if player[3] == input_country.title():
+            print(player[2] + "(" + player[0] + ")")
+
+
+# find player by name
+def findPlayerByName():
+    input_name = str(input("Enter player's name: "))
+    for player in players:
+        if player[2] == input_name.title():
+            print("\t" + player[2].upper() +
+                  "\n\tRanking: " + player[0] +
+                  "\n\tPoints: " + player[4] +
+                  "\n\tCountry: " + player[3])
+            
+
+
+print("====================================")
+findPlayerByRanking()
+print("====================================")
+findPlayerByCountry()
+print("====================================")
+findPlayerByName()
+print("====================================")
